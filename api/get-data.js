06e75005999374ca8b50690f6ff22b2cfe6eb632
Cowork Coach <asbjornkrogh@gmail.com>
2026-06-02 14:21:33 +0200

    debug: log yesterdayEntry keys to find correct field names

--- api/get-data.js
+++ api/get-data.js
@@ -92,6 +92,7 @@ export default async () => {
       steps:  yesterdayEntry.steps  ?? null,
       spo2:   round1(yesterdayEntry.spO2   ?? yesterdayEntry.spo2   ?? null),
       vo2max: round1(yesterdayEntry.vo2max ?? yesterdayEntry.vo2Max  ?? null),
+      _debug: Object.keys(yesterdayEntry),
     },
     events: rawEvents,
   });
